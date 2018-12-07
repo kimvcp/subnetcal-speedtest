818ed3a39a6bad73e3198df30f3f7d9f4b786368
Vichaphol Thamsuthikul <vichaphol.t@ku.th>
2018-12-07 11:17:30 +0700

    clean some code and modified index page

--- network/network/urls.py
+++ network/network/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('networkapps/',include('networkapps.urls')),
+    path('', RedirectView.as_view(url='/networkapps/')),
 ]
